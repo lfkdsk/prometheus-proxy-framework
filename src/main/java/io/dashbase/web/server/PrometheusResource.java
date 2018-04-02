42c4f2864a2283498e5822155b6ba431db7a8b6e
lfkdsk <lfk_dsk@hotmail.com>
2018-04-02 23:44:50 +0800

    add range api

--- src/main/java/io/dashbase/web/server/PrometheusResource.java
+++ src/main/java/io/dashbase/web/server/PrometheusResource.java
@@ -27,41 +27,41 @@ public final class PrometheusResource {
     private final static Logger logger = LoggerFactory.getLogger(PrometheusResource.class);
 
 
-//    {
-//        "numResults":0,
-//            "tableNames":[
-//        "_metrics"
-//    ],
-//        "excludeTableNames":[
-//
-//    ],
-//        "query":{
-//        "queryType":"equality",
-//                "col":"service",
-//                "value":"api",
-//                "equal":true
-//    },
-//        "timeRangeFilter":{
-//        "startTimeInSec":1522670146,
-//                "endTimeInSec":1522670206
-//    },
-//        "fields":[
-//
-//    ],
-//        "aggregations":{
-//        "cpu_usage":{
-//            "requestType":"numeric",
-//                    "col":"jvm.cpu.usage.percent.value",
-//                    "type":"avg"
-//        }
-//    },
-//        "useApproximation":false,
-//            "ctx":"8a4fa7e4dc86b085",
-//            "fetchSchema":false,
-//            "timeoutMillis":2147483647,
-//            "disableHighlight":true,
-//            "debugMode":0
-//    }
+    //    {
+    //        "numResults":0,
+    //            "tableNames":[
+    //        "_metrics"
+    //    ],
+    //        "excludeTableNames":[
+    //
+    //    ],
+    //        "query":{
+    //        "queryType":"equality",
+    //                "col":"service",
+    //                "value":"api",
+    //                "equal":true
+    //    },
+    //        "timeRangeFilter":{
+    //        "startTimeInSec":1522670146,
+    //                "endTimeInSec":1522670206
+    //    },
+    //        "fields":[
+    //
+    //    ],
+    //        "aggregations":{
+    //        "cpu_usage":{
+    //            "requestType":"numeric",
+    //                    "col":"jvm.cpu.usage.percent.value",
+    //                    "type":"avg"
+    //        }
+    //    },
+    //        "useApproximation":false,
+    //            "ctx":"8a4fa7e4dc86b085",
+    //            "fetchSchema":false,
+    //            "timeoutMillis":2147483647,
+    //            "disableHighlight":true,
+    //            "debugMode":0
+    //    }
     @Path("query")
     @GET
     public Response query(
@@ -69,14 +69,33 @@ public final class PrometheusResource {
             @QueryParam("time") String time,
             @QueryParam("timeout") long timeout
     ) throws Exception {
-//        long timeMillis = time == null ? System.currentTimeMillis() : DateUtils.timeNum(time);
-//        ResponseFactory factory = ResponseFactory.of(query, timeMillis);
-//        RapidRequest request = factory.createRequest();
-//        return factory.instantQuery();
+        //        long timeMillis = time == null ? System.currentTimeMillis() : DateUtils.timeNum(time);
+        //        ResponseFactory factory = ResponseFactory.of(query, timeMillis);
+        //        RapidRequest request = factory.createRequest();
+        //        return factory.instantQuery();
 
         ResponseFactory factory = ResponseFactory.of(query, 1522670199);
         RapidResponse response = httpService.query(factory.createRequest());
         System.out.println(response);
+        return null;
+    }
+
+    /**
+     * query=<string>: Prometheus expression query string.
+     * start=<rfc3339 | unix_timestamp>: Start timestamp.
+     * end=<rfc3339 | unix_timestamp>: End timestamp.
+     * step=<duration>: Query resolution step width.
+     * timeout=<duration>: Evaluation timeout. Optional. Defaults to and is capped by the value of the -query.timeout flag.
+     */
+    @Path("query_range")
+    @GET
+    public Response queryRange(
+            @QueryParam("query") String query,
+            @QueryParam("start") String start,
+            @QueryParam("end") String end,
+            @QueryParam("step") int step,
+            @QueryParam("timeout") long timeout
+    ) {
         return null;
     }
 }
