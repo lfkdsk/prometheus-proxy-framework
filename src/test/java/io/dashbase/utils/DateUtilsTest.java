2e50547b287e105795cbabb5a90e5af999a390af
lfkdsk <lfk_dsk@hotmail.com>
2018-04-05 17:09:15 +0800

    fix test

--- src/test/java/io/dashbase/utils/DateUtilsTest.java
+++ src/test/java/io/dashbase/utils/DateUtilsTest.java
@@ -3,13 +3,11 @@ package io.dashbase.utils;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DateUtilsTest {
 
     @Test
     void testDateTimestamp() {
         String timestamp = "2018-04-03T15:44:43.000Z";
-        Assert.assertEquals(1522770283000L, DateUtils.timeNum(timestamp));
+        Assert.assertEquals(1522770283L, DateUtils.timeNum(timestamp));
     }
 }
